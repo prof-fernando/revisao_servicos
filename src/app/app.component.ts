ea8b476213a1844364d672d09b0af38c35fe1e45
Fernando Luis <fernando@Fernandos-MacBook-Pro.local>
2024-06-27 14:56:48 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'revisao_servicos_http';
+  title = 'Professor';
+  cursos: any;
+
+  termoPesquisa = '';
+  constructor(private api: ApiService) {}
+
+  public buscarCursosIFFar() {
+    this.api.listarCursos(this.termoPesquisa).subscribe((dadosIFFar) => {
+      this.cursos = dadosIFFar;
+    });
+  }
 }
